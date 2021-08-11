f7aa7c388180f0fe9331b77e4176ce15454e35e7
Sid20-rgb <siddharthashakya37@gmail.com>
2021-08-10 22:22:55 -0700

    frame

--- gui_popup.py
+++ gui_popup.py
@@ -1,13 +1,30 @@
 from tkinter import *
 
 from tkinter import messagebox
+from PIL import ImageTk, Image
+
 root = Tk()
 
 root.title('Message Box')
 
+# def popup():
+#     messagebox.showinfo("This is my Popup", "Hello World!")
+#
+# Button(root, text = "Popup", command= popup).pack()
+
+# def popup():
+#     response = messagebox.askyesno("This is my Popup", "is it pooped up?")
+#     if response == 1:
+#         Label(root, text = "It is pooped up.").pack()
+#     else:
+#         Label(root, text = "It is pooped up but clicked NO.").pack()
+#
+# Button(root, text = "Popup", command = popup).pack()
+
+
 def popup():
-    messagebox.showinfo("This is my Popup", "Hello World!")
+    response = messagebox.showwarning("This is my Popup", "i am afraid it is not pooped up")
 
-Button(root, text = "Popup", command= popup).pack()
+Button(root, text = "Popup", command = popup).pack()
 
-mainloop()
+root.mainloop()
--- gui_seven.py
+++ gui_seven.py
@@ -4,11 +4,19 @@ from PIL import ImageTk, Image
 
 root = Tk()
 
-my_image = ImageTk.PhotoImage(Image.open("C:/Users/asus/desktop/ff.png"))
-my_label = Label(imaeg= my_image)
-my_label.pack()
+image = Image.open("C:/Users/asus/Downloads/eng.jpg")
+image = image.resize((450, 350), Image.ANTIALIAS)
+my_image = ImageTk.PhotoImage(image)
+
+image1 = Image.open("c:/Users/asus/Downloads/three.png")
+image1 = image1.resize((450, 350), Image.ANTIALIAS)
+my_image1 = ImageTk.PhotoImage(image1)
+
+image_list = [my_image, my_image1]
+my_label = Label(image= my_image1)
+my_label.grid(row = 0,  column= 0)
 
 button = Button(root, text = "Exit", command = root.quit, width = 20)
-button.pack()
+button.grid(row = 1, column = 0)
 
 root.mainloop()
