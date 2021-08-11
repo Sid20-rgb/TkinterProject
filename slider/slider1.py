a9d1cee23a509839025af8026b19598042090ba8
Sid20-rgb <siddharthashakya37@gmail.com>
2021-08-10 22:11:06 -0700

    slider

---
+++ slider/slider1.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+
+vertical = Scale(root, from_=0, to=500)
+vertical.pack()
+
+horizontal = Scale(root, from_= 0, to= 500, orient = HORIZONTAL)
+horizontal.pack()
+
+def slide():
+
+    my_label = Label(root, text = horizontal.get() + "x" + vertical.get()).pack()
+    root.geometry(str(horizontal.get()) + "x" + str(vertical.get()))
+
+my_button = Button(root, text = "Click", command = slide).pack()
+
+root.mainloop()
